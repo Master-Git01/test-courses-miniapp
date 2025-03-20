a17128ecbefda951d2bc447af275095dbeeb9834
Plazma06 <example@gmail.com>
2025-03-20 22:11:00 +0300

    first commit

---
+++ src/app/pages/feedback/feedback.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feedback',
+  imports: [],
+  template: ` <p>feedback works</p> `,
+  styles: [],
+})
+export class FeedbackComponent {}
---
+++ src/app/pages/shop/shop.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shop',
+  imports: [],
+  template: ` <h1>main shop</h1> `,
+  styles: [],
+})
+export class ShopComponent {}
